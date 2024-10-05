ed59182944eb5a7f2addaaea7ddca93f2cf1340a
sfr2645 <sfr2645@email.vccs.edu>
2024-10-05 03:41:17 +0000

    corrected code

--- main.cpp
+++ main.cpp
@@ -1,62 +1,47 @@
-//this program will calculate the radius, diameter, circumference, and area of a circle after the center point and any other point on the circle is inputted
-#include <cmath>
+//this program will output the inflation rates for two successive years and determine if the inflation trend is increasing or decreasing. It will also ask the user to input the current price of an item and its price from one year and two years ago
 #include <iostream>
-#include <iomanip>
-#include <cstdlib>
 
 using namespace std;
-const double PI = 3.1416;
 
-double distance(double,double,double,double);
-double radius(double,double,double,double);
-double circumference(double);
-double area(double);
-void getPoint(double&,double&);
+int computeInflation(int oneYearPrice, int twoYearPrice);
+double	computeInflation(double oneYearPrice, double twoYearPrice);
+double	computeInflation(double oneYearPrice, int twoYearPrice);
+double	computeInflation(int oneYearPrice, double twoYearPrice);
 
-int main() 
+int main()
+
 {
-double x1,y1,x2,y2,r;
+    int currPrice, oneYearPrice, twoYearPrice;
+    double oneYearInflation, twoYearInflation; 
+  
+    cout << "What is the current price?" << endl;
+    cin >> currPrice;
+    cout << "What is the  price of one year ago." << endl;
+    cin >> oneYearPrice;
+    cout << "What is the  price of two years ago?" << endl;
+    cin >> twoYearPrice;
 
-cout << fixed << showpoint << setprecision(2);
-cout << "Enter the Center coordinates for your circle\n";
-getPoint(x1,y1);
-cout <<"Enter the coordinates for a point located on your circle\n";
-getPoint(x2,y2);
-
-r = radius(x1,y1,x2,y2);
-
-cout << "Radius = " << r << std::endl;
-cout << "Diameter = " << 2*r << std::endl;
-cout << "Circumference = " << circumference(r) << std::endl;
-cout << "Area = " << area(r) << std::endl;
-
+  
+    oneYearInflation = (double)(currPrice - oneYearPrice) / oneYearPrice * 100;
+  
+    twoYearInflation = (double)(currPrice - twoYearPrice) / twoYearPrice * 100;
+  
+    if (oneYearInflation > twoYearInflation)
+    {
+        cout << "The inflation rate is increasing." << endl;
+    }
+    else if (oneYearInflation < twoYearInflation)
+    {
+        cout << "The inflation rate is decreasing." << endl;
+    }
+    else
+    {
+        cout << "There is no change in the inflation rate." << endl;
+    }
+  
+    cout << "One-year inflation: " << oneYearInflation << "%" << endl;
+  
+    cout << "Two-year inflation: " << twoYearInflation << "%" << endl;
+  
     return 0;
-}
-
-double distance(double x1,double y1, double x2, double y2)
-{
-  return sqrt(pow(x1-x2,2)+pow(y1-y2,2));
-}
-
-double radius(double x1,double y1,double x2, double y2)
-{
-  return distance(x1,y1,x2,y2);
-}
-
-double circumference(double r)
-{
-  return 2 * PI * r; 
-}
-
-double area(double r)
-{
-  return PI * pow(r,2);
-}
-
-void getPoint(double& x, double& y)
-{
-  cout << "x: ";
-  cin >> x;
-  cout << "y: ";
-  cin >> y;
 }
